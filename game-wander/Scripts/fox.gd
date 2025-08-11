e90b9db37e01b0bca789670421a666c3d4644614
KAKOnyx <kolesen@students.mags.school.nz>
2025-08-12 11:39:14 +1200

    labels work, flipping collision shapes

--- game-wander/Scripts/fox.gd
+++ game-wander/Scripts/fox.gd
@@ -5,14 +5,15 @@ extends CharacterBody2D
 
 const SPEED: float = 350.0
 
+
 var fragment_score : int = 0
 @export var ui : Node
 @export var second_ui: Node
 
 var flipped_x: bool = false
 @onready var anim = $AnimatedSprite2D as AnimatedSprite2D
-
-
+@onready var fox_col = $fox_collision as CollisionShape2D #colliding with objects
+@onready var play_col = $player_collision/player_collison_shape as CollisionShape2D
 
 
 
@@ -38,12 +39,20 @@ func _physics_process(_delta: float) -> void:
 	# Movement for animated sprite
 	if Input.is_action_pressed("ui_right"):
 		anim.play("right")
+		fox_col.rotation = deg_to_rad(0)
+		play_col.rotation = deg_to_rad(0)
 	elif Input.is_action_pressed("ui_left"):
 		anim.play("left")
+		fox_col.rotation = deg_to_rad(0)
+		play_col.rotation = deg_to_rad(0)
 	elif Input.is_action_pressed("ui_up"):
 		anim.play("up")
+		fox_col.rotation = deg_to_rad(90)
+		play_col.rotation = deg_to_rad(90)
 	elif Input.is_action_pressed("ui_down"):
 		anim.play("down")
+		fox_col.rotation = deg_to_rad(90)
+		play_col.rotation = deg_to_rad(90)
 	else:
 		anim.stop()
 
@@ -53,7 +62,6 @@ func _on_fragment_touched(area: Area2D) -> void:
 	if area.has_meta("fragment"):
 		fragment_score += 1
 		ui.text = str(fragment_score)
-		second_ui.text = str(fragment_score)
 		print(fragment_score)
 		area.queue_free()
 
